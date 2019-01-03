0b440c0c7ca33b8c070969f065fc558ef91667ee
José Moreno <jmoreno@ias.com.co>
2019-01-03 10:24:24 -0500

    [IMP]Adds Balanced Tree implementation but have a error in removeNode method

--- avl_tree.py
+++ avl_tree.py
@@ -84,7 +84,7 @@ class AVL(object):
             print("Right right heavy case II")
             return self.rotateLeft(node)
 
-        if balance < 1 and data > node.leftChild.data:
+        if balance > 1 and data > node.leftChild.data:
             print("Left right heavy case III")
             node.leftChild = self.rotateLeft(node.leftChild)
             return self.rotateRight(node)
@@ -109,7 +109,71 @@ class AVL(object):
         if node.rightChild:
             self.tranverseSort(node.rightChild)
 
+    def removeNode(self, data, node):
+        # ~ TODO: Corregir error al eliminar nodo cuando se calcula el height
+        if not node:
+            return node
+
+        if data < node.data:
+            node.leftChild = self.removeNode(data, node.leftChild)
+        elif data > node.data:
+            node.rightChild = self.removeNode(data, node.rightChild)
+        else:
+            if not node.leftChild and not node.rightChild:
+                print("Removing a leaf node...")
+                del node
+                return Node
+            if not node.leftChild:
+                print("Removing a node with a rightChild")
+                temp_node = node.rightChild
+                del node
+                return temp_node
+            elif not node.rightChild:
+                print("Removing a node with a leftChild")
+                temp_node = node.leftChild
+                del node
+                return temp_node
+
+            print("Removing node with two children...")
+            temp_node = self.getParent(node.leftChild)
+            node.data = temp_node.data
+            node.leftChild = self.removeNode(temp_node.data, node.leftChild)
+
+        if not node:
+            return node
+
+        node.height = max(self.calculate_height(node.leftChild), self.calculate_height(node.rightChild)) + 1
+        balance = self.calculate_balance(node)
+
+        if balance > 1 and self.calculate_balance(node.leftChild) >= 0:
+            return self.rotateRight(node)
+
+        if balance > 1 and self.calculate_balance(node.leftChild) < 0:
+            node.leftChild = self.rotateLeft(node.leftChild)
+            return self.rotateRight(node)
+
+        if balance < -1 and self.calculate_balance(node.rightChild) <= 0:
+            return self.rotateLeft(node)
+
+        if balance < -1 and self.calculate_balance(node.rightChild) > 0:
+            node.rightChild = self.rotateRight(node.rightChild)
+            return self.rotateLeft(node)
+
+        return node
+
+    def unlink(self, data):
+        if self.root:
+            self.root = self.removeNode(data, self.root)
+
+
 avl = AVL()
 avl.insert(10)
 avl.insert(20)
-avl.insert(30)
+avl.insert(5)
+avl.insert(6)
+avl.insert(15)
+avl.transverse()
+
+avl.unlink(15)
+avl.unlink(20)
+avl.transverse()
